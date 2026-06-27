c583b95d1b105616e609f6c12da2ad397729a3c0
Tuấn Thanh <tuanthanh9a3hihi@gmail.com>
2026-06-28 01:54:13 +0700

    fix: add missing production columns dynamically in UpdateOrderStatusEnum migration

--- backend/Migrations/20260627092706_UpdateOrderStatusEnum.cs
+++ backend/Migrations/20260627092706_UpdateOrderStatusEnum.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,13 +10,65 @@ namespace PolyBabyAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'IsReturnReceived')
+                BEGIN
+                    ALTER TABLE Invoices ADD IsReturnReceived bit NOT NULL DEFAULT 0;
+                END
+            ");
 
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'RefundMethod')
+                BEGIN
+                    ALTER TABLE Invoices ADD RefundMethod int NULL;
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'ReturnImageUrls')
+                BEGIN
+                    ALTER TABLE Invoices ADD ReturnImageUrls nvarchar(max) NULL;
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'ReturnReason')
+                BEGIN
+                    ALTER TABLE Invoices ADD ReturnReason nvarchar(max) NULL;
+                END
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'IsReturnReceived')
+                BEGIN
+                    ALTER TABLE Invoices DROP COLUMN IsReturnReceived;
+                END
+            ");
 
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'RefundMethod')
+                BEGIN
+                    ALTER TABLE Invoices DROP COLUMN RefundMethod;
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'ReturnImageUrls')
+                BEGIN
+                    ALTER TABLE Invoices DROP COLUMN ReturnImageUrls;
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'Invoices') AND name = 'ReturnReason')
+                BEGIN
+                    ALTER TABLE Invoices DROP COLUMN ReturnReason;
+                END
+            ");
         }
     }
 }
